abb1f9e7cfa8947ac7872ea39ce64247f3455613
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2020-05-27 22:34:46 +0530

    Done the code

--- src/App.js
+++ src/App.js
@@ -43,6 +43,7 @@ class App extends Component {
         <div>Signed In!</div>
         <button onClick={ () => firebase.auth().signOut()}>Sign Out</button>
       <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
+      <img alt="profile picture" src={firebase.auth().currentUser.photoURL}/>
         </span>
       ) : (
         <StyledFirebaseAuth 
