ed5dad466e5930bf03873ce9ceba7792a947e78e
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2020-05-27 21:54:57 +0530

    added authentication providers

--- src/App.js
+++ src/App.js
@@ -5,7 +5,20 @@ import firebase from "firebase"
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
 
 class App extends Component {
+
   state = { isSignedIn: false }
+  uiConfig = {
+    signInFlow: "popup",
+    signInOption: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  }
   render() {
   return (
     <div className="App">
